0c87a6c925eceb03020de563b98263b53e9eea28
unknown <monjurul.gakk@gmail.com>
2021-07-12 22:09:29 +0600

    Ini

---
+++ MessageProducer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MessageProducer
+{
+   static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("message-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var messsage = new { Name = "Message Produced", Description = "Rabbit MQ 101 Message Queue" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messsage));
+            channel.BasicPublish("", "message-queue", null, body);
+            
+        }
+    }
+}
